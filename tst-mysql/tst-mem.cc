df83ae749fc1b262fac256611bd275ade75c1eb4
morgan-139117 <com.morgan@gmail.com>
2015-05-05 04:50:07 -0500

    OSv_KVM_MEM_SWAP_DEMO

--- tst-mysql/tst-mem.cc
+++ tst-mysql/tst-mem.cc
@@ -1,9 +1,40 @@
 /*
- * Copyright (C) 2014 Cloudius Systems, Ltd.
- *
- * This work is open source software, licensed under the terms of the
- * BSD license as described in the LICENSE file in the top-level directory.
- */
+Dell 5547. 4G memory. Ubuntu 14.04 x64. Swappiness = 95 lol
+
+Use swap file :
+
+1. dd if=/dev/zero of=/media/fasthdd/swapfile.img bs=1024 count=1M
+
+2. mkswap /media/fasthdd/swapfile.img
+
+3. # Add this line to /etc/fstab 
+   /media/fasthdd/swapfile.img swap swap sw 0 0
+
+4. swapon /media/fasthdd/swapfile.img
+
+Use Osv Test case 
+
+1. set command line to Image: ./scripts/run.py -e "tests/tst-mem.so 100"  
+
+2. launch OSv with 1G mem:
+
+ qemu-system-x86_64  -m 1G -smp 4 -device virtio-blk-pci,id=blk0,bootindex=0,drive=hd0,scsi=off -drive file=/home/abc/osv7/build/release/usr.img,if=none,id=hd0,aio=native,cache=none -netdev user,id=un0,net=192.168.122.0/24,host=192.168.122.1 -device virtio-net-pci,netdev=un0  -device virtio-rng-pci -enable-kvm -cpu host,+x2apic -chardev stdio,mux=on,id=stdio,signal=off -mon chardev=stdio,mode=readline,default -device isa-serial,chardev=stdio
+
+Larger than 1G recive following error when launching 4 instances
+Cannot set up guest memory 'pc.ram': Cannot allocate memory
+
+Result
+
+As 3 out of 4G memory was available, each machine held 1G in mem when three instances were launched. 
+
+After all 4 instances were launched, memory requests exceeded the capacity, large part of memory held by first two instanced were swapped out.
+
+And Swap was full.
+
+
+
+*/
+
 
 #include <iostream>
 #include <string>
@@ -11,7 +42,7 @@
 #include <stdio.h>
 #include <math.h>
 #include <unistd.h>
-
+#include <sstream>
 unsigned int microseconds;
 
 struct sysinfo myinfo;
@@ -22,7 +53,7 @@ unsigned long freeram_bytes;
 
 class Node {
 public:
-	Node(char* abc) {
+	Node(char* abc) { 
 		value = abc;
 		next = NULL;
 	}
@@ -57,31 +88,28 @@ public:
 
 	void show(){
 		cur = head;
-std::cout<<"c";
-
+		std::cout<<"showed";
+		int i = 0;
 		while(cur != NULL){
 			if(cur->value != NULL)
-			std::cout<<cur->value;
+			i++;			
+			//std::cout<<cur->value;
 			cur = cur->next;
 		}
-std::cout<<"c";
+
 	}
 
 };
 
 void newmem(LeakingList& charList, long size) {
-	std::string name;
-
 
 	char* largechar = new char[size]; // have to cheat the compiler this way lol
 
 	for(int i = 0;i < size; i++){
 
-		largechar[i] = 'a' ;
+			largechar[i] = 'a' ;
 
-	}
-	
-
+		}
 
 	charList.add(largechar);
 
@@ -94,29 +122,34 @@ void newmem(LeakingList& charList, long size) {
 	float rp = sqrt(1 - (float) freeram_bytes / (float) total_bytes);
 
 	freeram_bytes = myinfo.mem_unit * myinfo.freeram;
-	printf("resource pressure %f ", rp);
-
-
+	printf("resource pressure %f , lsize %d", rp,charList.size());
 
 }
 
 int main(int ac, char** av)
 
 {
-	microseconds = 50000000;
+
+   	double limit = 0.0;
+
+		std::istringstream ss(av[1]);
+
+		ss >> limit;
+
+
+	microseconds = 5000000;
 	LeakingList charList;
-	long size = 1;
-	for (int i = 0; i < 800;i++) {
+	long size = 10240240l;
+	for (int i = 0; i < (int)limit;i++) {
 		newmem(charList, size);
-		size += 10240;
 	}
 
-	for (int i = 0; i < 100;i++) {
+	for (int i = 0; i < 30;i++) {
 		usleep(microseconds);
 		charList.show();
-		
+		getchar();
 	}
+	
 
 	return 0;
 }
-
